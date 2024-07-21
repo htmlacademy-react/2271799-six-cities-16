befa8b83bad7005bf74748d37a95ac958ab24aaf
valeriyasv <lerushka.sav@gmail.com>
2024-07-20 22:27:08 -0500

    adds react-helmet-async

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
-
+import {HelmetProvider} from 'react-helmet-async';
 import Main from '../../pages/main/main';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import Login from '../../pages/login/login';
@@ -14,34 +14,36 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={<Main placesCount={placesCount} />}
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<Login />}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={
-            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth} >
-              <Favorites />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={AppRoute.Offer}
-          element={<Offer />}
-        />
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={<Main placesCount={placesCount} />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<Login />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth} >
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<Offer />}
+          />
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,8 +1,12 @@
+import { Helmet } from 'react-helmet-async';
 import {Link} from 'react-router-dom';
 
 function NotFound(): JSX.Element {
   return (
     <>
+      <Helmet>
+        <title>6 городов. Страница не найдена</title>
+      </Helmet>
       <h1>404. Page not found</h1>
       <Link to="/">Вернуться на главную</Link>
     </>
