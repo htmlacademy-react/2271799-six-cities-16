324bda4027cc902d3990f45082247a9baabb98c8
valeriyasv <lerushka.sav@gmail.com>
2024-07-20 22:11:31 -0500

    replaces Route with PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 
 import Main from '../../pages/main/main';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
 import Offer from '../../pages/offer/offer';
 import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placesCount: number;
@@ -25,7 +26,11 @@ function App({placesCount}: AppProps): JSX.Element {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<Favorites />}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth} >
+              <Favorites />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Offer}
