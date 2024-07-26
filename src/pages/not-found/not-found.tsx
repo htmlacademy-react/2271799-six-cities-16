41f4f1e8c44653219ac31adaee8b15f4af9e0858
valeriyasv <lerushka.sav@gmail.com>
2024-07-25 21:59:40 -0500

    adds addresses to links

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,15 +1,33 @@
 import { Helmet } from 'react-helmet-async';
 import {Link} from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function NotFound(): JSX.Element {
   return (
-    <>
+    <div className="page page--gray page--login">
       <Helmet>
         <title>6 городов. Страница не найдена</title>
       </Helmet>
-      <h1>404. Page not found</h1>
-      <Link to="/">Вернуться на главную</Link>
-    </>
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">404. Страница не найдена</h1>
+            <form className="login__form form" action="#" method="post">
+              <Link className="login__submit form__submit button" to={AppRoute.Main}>
+                Вернуться на главную
+              </Link>
+            </form>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
 export default NotFound;
