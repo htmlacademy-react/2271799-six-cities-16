87b0c91d7202dcac15ec1b5846b380670be12614
valeriyasv <lerushka.sav@gmail.com>
2024-08-15 22:03:32 -0500

    adds handling server interaction errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
+import ErrorMessage from './components/error/error-message';
 import { offer } from './mocks/offer';
 import { reviews } from './mocks/reviews';
 import { store } from './store';
 import { fetchOffersAction } from './store/api-action';
-import ErrorMessage from './components/error/error-message';
+import {checkAuthAction} from './store/api-action';
 
 store.dispatch(fetchOffersAction());
+store.dispatch(checkAuthAction());
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
